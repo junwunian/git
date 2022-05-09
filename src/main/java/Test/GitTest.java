dba57d83bd9b2022543feac24fe1b24152a45c08
dengmengjun <dengmengjun@qq.com>
2022-05-09 10:32:40 +0800

    hot-fix commit1

--- src/main/java/Test/GitTest.java
+++ src/main/java/Test/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello 邓哥");
         System.out.println("修改");
+        System.out.println("hot-fix修改");
     }
 }
