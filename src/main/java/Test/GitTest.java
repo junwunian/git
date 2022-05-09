a7d554ae4cd25d4551f023ecc81e9a0be26ad405
dengmengjun <dengmengjun@qq.com>
2022-05-09 10:59:13 +0800

    master commit6

--- src/main/java/Test/GitTest.java
+++ src/main/java/Test/GitTest.java
@@ -3,9 +3,9 @@ package Test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello 等牛逼哥");
+        System.out.println("我到这个位置会怎样");
         System.out.println("修改");
         System.out.println("hot-fix修改");
-        System.out.println("我就没怕过谁");
         System.out.println("会不会冲突呢");
         System.out.println("我真的会害怕");
         System.out.println("Laia");
